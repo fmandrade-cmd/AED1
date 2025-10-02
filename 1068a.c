90aa90d34867b63a9dc8e2afa48c41a70a63d415
fmandrade-cmd <fm.andrade@unifesp.br>
2025-10-02 14:39:39 -0300

    Update 1068a.c

--- 1068a.c
+++ 1068a.c
@@ -1,38 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 
-int bemFormada(char s[]){
+int bemFormada(char *p){
     //char *p; //Cria um vetor
     int t, i;
-
-    //n = strlen(s);
-    //p = malloc(n * sizeof(char)); //Determina o tamanho do vetor
     t = 0;
-    for(i = 0; s[i] != '\0'; i++){
-        if(s[i] == '(') t++;  
-        if(s[i] == ')') t--;
+    for(i = 0; p[i] != '\0'; i++){
+        if(p[i] == '(') t++;  
+        if(p[i] == ')') t--;
+        //Verifica erro do tipo: ')(' seria considerado CORRETO
+        if(t < 0) return 0;
     }
-    if(t == 0) return 1;
-    return 0;
+    return (t == 0); //1 para verdadeiro e 0 para falso
 }
 
 int main(){
-
-    char v[1000];
+    //Aloca memória e cria um vetor
+    char *p;
+    p = (char *)malloc(1001 * sizeof(char));
+    
     int ref, N;
     int count = 0;
-    
+    //Número de espressões
     scanf("%d", &N);
 
     while(count < N){
-        scanf("%s", v);
-        ref = bemFormada(v);
+        //Lê uma String com até 1000 caracteres
+        scanf("%1000s", p);
+        //Verifica a expresão
+        ref = bemFormada(p);
         if(ref == 1) 
             printf("correct");
         else
             printf("incorrect");
         count++;
+        
     }
+    //Libera a mamória alocada
+    free(p);
 
     return 0;
 }
